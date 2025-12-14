40b511c3a94d593987ea13f71a6e6d60f017d2b7
Firas Mosbehi <firas.mosbehi@the-qa-company.com>
2025-12-14 18:16:10 +0100

    allow ssl encryption in db connection

--- backend/db.go
+++ backend/db.go
@@ -30,7 +30,7 @@ func initDB() {
 	dbName := getEnv("DB_NAME", "taskboard")
 
 	dsn := fmt.Sprintf(
-		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=require",
 		dbHost, dbPort, dbUser, dbPassword, dbName,
 	)
 
